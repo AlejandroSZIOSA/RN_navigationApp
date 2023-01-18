5cb77e241470cbd0df118511ff3ab1563ac5b188
AlejandroSZIOSA <ggato33@gmail.com>
2023-01-18 16:46:11 +0100

    Create Components 1

---
+++ models/category.js
@@ -0,0 +1,9 @@
+//CLASS DEFINITION
+class Category {
+  constructor(id, title, color) {
+    this.id = id;
+    this.title = title;
+    this.color = color;
+  }
+}
+export default Category;
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,29 @@
+import { StyleSheet,FlatList } from "react-native";
+
+import CategoryGridTile from "../components/CategoryGridTile";
+
+import { CATEGORIES } from "../data/dummy-data";
+
+function renderCategoryItem(itemData){
+    return( 
+    <CategoryGridTile 
+      title={itemData.item.title}
+      color={itemData.item.color}  
+    />
+    )
+}
+function CategoriesScreen(){
+    return( 
+    <FlatList 
+      data={CATEGORIES} 
+      keyExtractor={(item) => item.id}
+      renderItem=  {renderCategoryItem}  
+    />
+    );
+        }
+
+export default CategoriesScreen;
+
+const styles = StyleSheet.create({
+
+})

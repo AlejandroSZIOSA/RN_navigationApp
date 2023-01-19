dbb7693646312b9e4750199c8a82c3960ec372dc
AlejandroSZIOSA <ggato33@gmail.com>
2023-01-19 14:41:34 +0100

    Display Meals Overview

--- models/meal.js
+++ models/meal.js
@@ -2,7 +2,7 @@
 class Meal {
   constructor(
     id,
-    categoryIds,
+    categoryIds, //is an array
     title,
     affordability,
     complexity,
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,14 +1,28 @@
 
-import { View,StyleSheet,Text } from "react-native";
+import { View,StyleSheet,Text,FlatList } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 //Object destructing because this component is a child of a stack.screen
 function MealsOverviewScreen({route}){
     //extracting categoryId 
     const catId = route.params.categoryId;
+    const displayedMeals = MEALS.filter((mealItem) => {
+        //It is true if there is a match
+        return mealItem.categoryIds.indexOf(catId) >=0;
+    } );
+    function renderMealItem(itemData){
+        return(
+            <MealItem title={itemData.item.title}/> //passin data prop
+        )
+    }
     return(
         <View style={styles.container}>
-            <Text>Meals Overview Screen {catId}</Text>
+            <FlatList
+             data ={displayedMeals}
+             keyExtractor={(item)=> item.id}
+             renderItem={renderMealItem} //passing params automatic :)
+            />
         </View>
     );
 };
@@ -19,5 +33,4 @@ const styles = StyleSheet.create({
         flex: 1,
         padding:16
     }
-
 })

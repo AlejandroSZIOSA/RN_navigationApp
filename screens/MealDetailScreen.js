271ff60c9dba2f459fbe7f5513e6d765c3b403e5
AlejandroSZIOSA <ggato33@gmail.com>
2023-01-29 16:02:53 +0100

    new reusable component

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,28 @@
-import { Text } from "react-native"
+import { Text, View,Image } from "react-native"
+import MealDetails from "../components/MealDetails";
+import { MEALS } from "../data/dummy-data";
 //route give access to the parent component and a reference
 function MealDetailScreen({route}){
     const mealId =  route.params.mealId; // this comes from MealsItem
-    return<Text> Meal details ({mealId})</Text>
+    const selectedMeal = MEALS.find((meal)=> meal.id === mealId );
+    return <View>
+        <Image source={{uri:selectedMeal.imageUrl}}  />
+
+        <Text> {selectedMeal.title}</Text>
+        
+        <MealDetails 
+          duration={selectedMeal.duration}
+          complexity={selectedMeal.complexity}
+          affordability={selectedMeal.affordability}
+          />
+
+        <Text> Ingredients </Text>
+        {selectedMeal.ingredients.map(ingredient => 
+            <Text key={ingredient}> {ingredient}</Text>)}
+        <Text> steps </Text>
+        {selectedMeal.steps.map(step => 
+            <Text key={step}> {step}</Text>)}
+
+    </View>
 }
 export default MealDetailScreen;
